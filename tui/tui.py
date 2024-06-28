57065484cd9067c4510a0aeb8889b63dec41de73
Markus <92539850+mabezi@users.noreply.github.com>
2024-06-28 15:02:51 +0200

    expand simple UI

--- tui/tui.py
+++ tui/tui.py
@@ -10,7 +10,7 @@ def open_main():
                 "or press \"Help\" to get a brief description of the game.",
                 "",
                 Splitter(
-                ["New Game", lambda *_: print("new game")],
+                ["New Game", lambda *_: draw_game(["1", "2"], ["K", "B"])],
                         ["Help", lambda *_: open_help()],
                         ["Exit", lambda *_: exit(0)],
                 ),
@@ -43,6 +43,77 @@ def open_help():
                 box="SINGLE",
             )
             .set_title("[210 bold]Help")
+            .center()
+        )
+
+        manager.add(window)
+
+
+def draw_game(open_cards: [], your_cards: [], your_draw: bool = True):
+    oc = " - "
+    for card in open_cards:
+        oc += card + " - "
+    yc = " - "
+    for card in your_cards:
+        yc += card + " - "
+    with ptg.WindowManager() as manager:
+        window = (
+            Window(
+                "Du bist am Zug!" if your_draw else "Warten auf deine Gegner...",
+                Container(
+                    "Aufgedeckte Karten:",
+                    Container(oc)
+                ),
+                Container(
+                    "Dein Blatt:",
+                    Container(yc)
+                ),
+                "",
+                ["Karte legen", lambda *_: draw(your_cards)] if your_draw else [],
+                width=60,
+                box="SINGLE",
+            )
+            .set_title("[210 bold]InGame")
+            .center()
+        )
+
+        manager.add(window)
+
+
+def draw(your_cards):
+    c = []
+    for card in your_cards:
+        c.append(["Lege \"" + card + "\"", lambda *_: print(card)])
+
+    with ptg.WindowManager() as manager:
+        window = (
+            Window(
+                "Du bist am Zug!",
+                *c,
+                width=60,
+                box="SINGLE",
+            )
+            .set_title("[210 bold]InGame")
+            .center()
+        )
+
+        manager.add(window)
+
+
+def draw_end(win: bool):
+    with ptg.WindowManager() as manager:
+        window = (
+            Window(
+                "",
+                Container(
+                    "Du hast gewonnen!" if win else "Du hast verloren :(."
+                ),
+                "",
+                ["Back To Main Menu", lambda *_: open_main()],
+                width=60,
+                box="SINGLE",
+            )
+            .set_title("[210 bold]End")
             .center()
         )
 
