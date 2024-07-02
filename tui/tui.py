2356c669488cc17612e76bdf186447730d9fd3a9
Markus <92539850+mabezi@users.noreply.github.com>
2024-07-02 11:16:32 +0200

    expand simple UI

--- tui/tui.py
+++ tui/tui.py
@@ -10,11 +10,12 @@ def open_main():
                 "or press \"Help\" to get a brief description of the game.",
                 "",
                 Splitter(
-                ["New Game", lambda *_: draw_game(["1", "2"], ["K", "B"])],
+                ["New Game", lambda *_: create_game(["blabla", "test"])], # ToDo Funktion zum Starten hinzufügen
+                        ["Find Games", lambda *_: find_games()],
                         ["Help", lambda *_: open_help()],
                         ["Exit", lambda *_: exit(0)],
                 ),
-                width=60,
+                width=100,
                 box="DOUBLE",
             )
             .set_title("[210 bold]Main Menu")
@@ -49,27 +50,92 @@ def open_help():
         manager.add(window)
 
 
-def draw_game(open_cards: [], your_cards: [], your_draw: bool = True):
+def find_games():
+    # ToDo: Add Method to get all open games
+    games = ["A", "B", "C", "Kekse"]
+    game_buttons = []
+    for game in games:
+        game_buttons.append([game, lambda *_: print("Start " + game)]) # ToDo: Instead of print add the join function
+
+    with ptg.WindowManager() as manager:
+        window = (
+            Window(
+                "",
+                Container(
+                    "Join one of the following games"
+                ),
+                Container(*game_buttons),
+                "",
+                ["Back To Main Menu", lambda *_: open_main()],
+                width=100,
+                box="SINGLE",
+            )
+            .set_title("[210 bold]Join Game")
+            .center()
+        )
+
+        manager.add(window)
+
+
+def create_game(players):
+    # Aufrufen, sobald Spiel erstellt wurde...
+    player_list = []
+    for player in players:
+        player_list.append(player)
+
+    with ptg.WindowManager() as manager:
+        window = (
+            Window(
+                "",
+                Container(
+                    "Waiting for players..."
+                ),
+                Container(
+                    "Player list:",
+                    *player_list
+                ),
+                "",
+                ["Back To Main Menu", lambda *_: open_main()],
+                width=100,
+                box="SINGLE",
+            )
+            .set_title("[210 bold]Create Game")
+            .center()
+        )
+
+        manager.add(window)
+
+
+def draw_game(open_cards: [], your_cards: [], chips: int = 0, your_draw: bool = True,
+              raise_chips: bool = True, check: bool = True, fold: bool = True, call: bool = True):
     oc = " - "
     for card in open_cards:
         oc += card + " - "
     yc = " - "
     for card in your_cards:
         yc += card + " - "
+
     with ptg.WindowManager() as manager:
         window = (
             Window(
-                "Du bist am Zug!" if your_draw else "Warten auf deine Gegner...",
+                "It's your turn!" if your_draw else "Waiting for opponent...",
                 Container(
-                    "Aufgedeckte Karten:",
+                    "Open Cards:",
                     Container(oc)
                 ),
                 Container(
-                    "Dein Blatt:",
+                    "Your cards",
                     Container(yc)
                 ),
+                "Your Chips: " + chips,
                 "",
-                ["Karte legen", lambda *_: draw(your_cards)] if your_draw else [],
+                Container(
+                    "Actions:",
+                    ["Bet", lambda *_: bet(your_cards)] if raise_chips else "(Bet not available)",
+                    ["Check", lambda *_: print("Todo")] if check else "(check not available)",
+                    ["Fold", lambda *_: print("Todo")] if fold else "(fold not available)",
+                    ["Call", lambda *_: print("Todo")] if call else "(call not available)",
+                ) if your_draw else "",
                 width=60,
                 box="SINGLE",
             )
@@ -80,24 +146,8 @@ def draw_game(open_cards: [], your_cards: [], your_draw: bool = True):
         manager.add(window)
 
 
-def draw(your_cards):
-    c = []
-    for card in your_cards:
-        c.append(["Lege \"" + card + "\"", lambda *_: print(card)])
-
-    with ptg.WindowManager() as manager:
-        window = (
-            Window(
-                "Du bist am Zug!",
-                *c,
-                width=60,
-                box="SINGLE",
-            )
-            .set_title("[210 bold]InGame")
-            .center()
-        )
-
-        manager.add(window)
+def bet(your_credits: int = 0):
+    pass
 
 
 def draw_end(win: bool):
@@ -106,7 +156,7 @@ def draw_end(win: bool):
             Window(
                 "",
                 Container(
-                    "Du hast gewonnen!" if win else "Du hast verloren :(."
+                    "You have won! Congratulation!" if win else "You have lost! F. :(."
                 ),
                 "",
                 ["Back To Main Menu", lambda *_: open_main()],
